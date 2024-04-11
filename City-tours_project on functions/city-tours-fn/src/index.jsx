a8ea322305dcc911049aa5114ad410a6463eebaf
alex1001A <alexiordanov1998@gmail.com>
2024-04-11 20:24:27 +0300

    add photos and tourData

---
+++ City-tours_project on functions/city-tours-fn/src/index.jsx
@@ -0,0 +1,15 @@
+// React hooks
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components
+import App from './App';
+
+// Styles
+import './style/main.scss';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
