ef6cc143274258c2c1e7b2d69d21c700d1786828
alex1001A <alexiordanov1998@gmail.com>
2024-04-23 16:47:45 +0300

    add input

--- City-tours_project on functions/city-tours-fn/src/Pages/Tours.jsx
+++ City-tours_project on functions/city-tours-fn/src/Pages/Tours.jsx
@@ -1,3 +1,6 @@
+//React components/hooks
+import { useRef } from "react";
+
 //Component
 import Tour from "../Components/Tour/Tour";
 
@@ -6,11 +9,19 @@ import "../style/pages/tours-page.scss";
 
 export default function Tours({ tours, setTours }) {
 
+  const inpRef = useRef(null)
+
+  function showMessage() {
+    const searchInput = inpRef.current.value.toLowerCase()
+    const searchedTours = tours.filter(tour => searchInput == tour.city)
+    setTours(searchedTours)
+  }
+
   return (
     <>
       <section className="hero-tours">
         <div className="container hero-tours__container">
-
+          <input ref={inpRef} type="text" placeholder="type your favourite city..." onChange={showMessage}/>
         </div>
       </section>
       <section className="tours">
