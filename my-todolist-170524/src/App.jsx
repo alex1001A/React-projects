5086b67660e6822b45cfd5e46c658b76d28acd17
alex1001A <alexiordanov1998@gmail.com>
2024-05-22 22:31:09 +0300

    sort phones

--- my-todolist-170524/src/App.jsx
+++ my-todolist-170524/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React from "react";
+
+import Products from './components/Products'
 
 //'https://jsonplaceholder.typicode.com/users'
 
 export default function App() {
-  return <div></div>;
+  return <div className="App">
+    <Products/>
+  </div>;
 }
--- my-todolist-170524/src/components/Counter/Counter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Counter({ count }) {
-  return <h1>{count}</h1>;
-}
-
-export default Counter;
