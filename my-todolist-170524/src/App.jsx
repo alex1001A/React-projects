c0214e4dcd31702538d2ab2bb26a902e5db3be14
alex1001A <alexiordanov1998@gmail.com>
2024-05-22 20:50:14 +0300

    do my todo with counter

--- my-todolist-170524/src/App.jsx
+++ my-todolist-170524/src/App.jsx
@@ -1,25 +1,9 @@
 import "./App.css";
 
 import React, { useState } from "react";
-import Counter from "./components/Counter/Counter";
-import Button from "./components/Counter/Button";
 
 //'https://jsonplaceholder.typicode.com/users'
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-
-  return (
-    <div>
-      <Counter count={count} />
-      <Button upCount={increment} />
-      <Button upCount={increment} />
-      <Button upCount={increment} />
-      <Button upCount={increment} />
-    </div>
-  );
+  return <div></div>;
 }
